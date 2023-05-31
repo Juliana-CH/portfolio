2c7d28a1fb515720c4be5b61f29d99a2aa2c2770
Juliana <ju.hazim@gmail.com>
2023-05-31 10:30:45 +0200

    Deleting node_modules to fix a bug.

---
+++ src/views/index/_index.jsx
@@ -0,0 +1,75 @@
+<header className="header-container">
+    <nav className="header-menu">
+        <li className="header-menu-list">
+            <ul>Início</ul>
+            <ul>Sobre mim</ul>
+            <ul>Experiências</ul>
+            <ul>Projetos</ul>
+            <ul>Contato</ul>
+        </li>
+    </nav>
+</header>
+
+<section className="hello">
+    <h1>
+        OLÁ, EU SOU JULIANA HAZIM!
+    </h1>
+
+    <h2>
+        Brasileira, Administradora de Empresas em transição de carreira, 
+        adicionando conhecimentos para me tornar uma Desenvolvedora Front-End!
+    </h2>
+
+    <button className="btn-knowme">
+        Me conheça!
+    </button>
+</section>
+
+<section className="aboutme">
+    <h2>Sobre<span className="titles-second-word">mim</span></h2>
+    <p>
+        Sou brasileira e atualmente vivo em Valencia, Espanha. 
+        Sou casada com João, meu parceiro de vida e mãe da Maria Antônia 
+        e do Romeo, que me ensinam diariamente sobre amor.
+        Adoro jogar beach tennis, compartilhar conhecimento e contribuir 
+        para que todos tenham oportunidades semelhantes. 
+        Também me interesso muito por alimentação saudável, consumo 
+        consciente e julgo disciplina positiva uma ferramenta útil para 
+        toda a sociedade!
+        Tenho alguns anos de experiência em publicidade, entretanto, com 
+        o nascimento dos meus filhos, escolhi me dedicar exclusivamente 
+        à eles, mesmo sabendo que o desafio do retorno seria enorme. 
+        Chegou a hora! Estou segura e animada para equilibrar esses 
+        interesses e retomar minha carreira profissional!
+    </p>
+</section>
+
+<section className="skills">
+    <h2>Soft<span className="titles-second-word">skills</span></h2>
+    <h3 className="skills-title">Adaptação</h3>
+        <p>Já vivi em 6 cidades, 4 estados e 2 países diferentes!</p>
+    <h3 className="skills-title">Organização</h3>
+        <p>Ministrei cursos focados em organização, otimização da rotina e, 
+        por consequência, da qualidade de vida das pessoas.</p>
+    <h3 className="skills-title">Comprometimento & Orientação à negócio</h3>
+        <p>“Juliana é uma excelente profissional: Excelente motivação e 
+            energia, senso de negócios com fortes habilidades de gestão. 
+            Grande iniciativa e condução. Eu a recomendo para todos os 
+            projetos, especialmente aqueles em que a dedicação é a chave.”</p>
+        <p>Jonas Mendes Constante | PhD, Strategy & Innovation - Ports and Logistics, Senior Consultant</p>
+    
+    <div className="other-skills-list">
+    <h3 className="skills-title">Colaboração</h3>
+    <h3 className="skills-title">Entusiasmo</h3>
+    <h3 className="skills-title">Responsabilidade</h3>
+    <h3 className="skills-title">Autogestão</h3>
+    <h3 className="skills-title">Trabalho em equipe</h3>
+    <h3 className="skills-title">Foco em solução</h3>
+    </div>
+
+</section>
+
+<section className="timeline">
+
+    
+</section>
